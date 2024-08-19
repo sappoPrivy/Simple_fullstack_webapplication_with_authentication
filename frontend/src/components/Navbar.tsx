82efb1064ebe2a80269ad30261914293787f0ff8
Choedon <choedon@kth.se>
2024-08-19 18:45:47 +0200

    Work on the home page and navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "/home/sappo/simple_fullstack_app/frontend/src/styles/Navbar.css";
+
+interface Props {
+  activeChild: string;
+}
+export const Navbar = ({ activeChild }: Props) => {
+  // List of all pages
+  // Then map the pages
+  // If pagename matches activeChild -> add active to the className
+  const pages = ["home", "profile", "larum ipsum"];
+
+  return (
+    <div className="nav-container">
+      <div className="sidebar">
+        {pages.map((page, index) =>
+          page == activeChild ? (
+            <a
+              aria-current="true"
+              key={index}
+              className="active"
+              href={"http://127.0.0.1:5173/" + page}
+            >
+              {page}
+            </a>
+          ) : (
+            <a key={index} href={"http://127.0.0.1:5173/" + page}>
+              {page}
+            </a>
+          )
+        )}
+      </div>
+      <div className="content">
+        <h1>Main Content</h1>
+        <p>
+          This is the main content area. The sidebar is positioned on the right.
+        </p>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,20 +2,30 @@ import React from "react";
 import { Button } from "../components/Button";
 import { useUser } from "../UserContext";
 import { useNavigate } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
 
-export const Home = () => {
-  const { logout } = useUser();
+export const Home: React.FC = () => {
+  const { user, logout } = useUser();
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
     navigate("/"); // Redirect to Login page
   };
+
+  if (!user) {
+    navigate("/");
+    return null;
+  }
   return (
     <>
-      <Button color="primary" type="submit" onClick={handleLogout}>
-        Log out
-      </Button>
+      <div>
+        <h4>{user.username}</h4>
+        <Button color="primary" type="button" onClick={handleLogout}>
+          Log out
+        </Button>
+      </div>
+      <Navbar activeChild="home"></Navbar>
     </>
   );
 };
